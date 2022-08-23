5465434a80e95408b5134a9cb5872fb16272abbc
YueSong <songyue19960927@gmail.com>
2022-08-23 09:36:17 +0200

    Update YUE_test_VP_pairs.sh

--- StyleGAN2/scripts/YUE_test_VP_pairs.sh
+++ StyleGAN2/scripts/YUE_test_VP_pairs.sh
@@ -16,6 +16,10 @@ python closed_form_factorization.py --out ./factor/factor_b8_10000.pt ./checkpoi
 
 wait
 
+python ppl_sefa.py ./checkpoint_FFHQ_loadd_all_FULL_wo_loss_b8_iter560000/560000.pt --factor ./factor/factor_b8_10000.pt --ortho_id -2 --sampling full \
+
+wait
+
 python ppl.py ./checkpoint_FFHQ_loadd_all_FULL_wo_loss_b8_iter560000/560000.pt --ortho_id -2 --sampling full \
 
 wait
