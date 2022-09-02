850e2ed7f77d751b53ec9574d24ca4153801a544
YueSong <songyue19960927@gmail.com>
2022-09-02 15:07:38 +0200

    Add files via upload

--- StyleGAN2/scripts/YUE_train1.sh
+++ StyleGAN2/scripts/YUE_train1.sh
@@ -9,13 +9,31 @@ bash
 
 cd /nfs/data_chaos/ysong/StyleGAN2/
 
-conda activate ood
+conda activate eg3d
 
 python -m torch.distributed.launch \
       --nproc_per_node=1 --master_port=9091 \
-      train_256.py --batch 4 /nfs/data_lambda/jzhang/github/siggraph/stylegan2-pytorch/data/ffhq256 \
-      --ckpt ./checkpoint_FFHQ/550000.pt --size 256 --ortho_id -2 --iter 552000 \
-      --checkpoints_dir checkpoint_FFHQ_loadd_all_FULL_wo_loss_b4 \
-      --sample_dir samplev_loadd_all_FULL_wo_loss_b4 --loadd --training_FULL --diag_size 10 &
+      train_256.py --batch 8 /nfs/data_lambda/jzhang/github/siggraph/stylegan2-pytorch/data/ffhq256 \
+      --ckpt ./checkpoint_FFHQ/550000.pt --size 256 --ortho_id -3 --iter 552000 \
+      --checkpoints_dir checkpoint_FFHQ_lr0.01_b8_id3 \
+      --sample_dir samplev_lr0.01_b8_id3 --loadd --training_FULL --diag_size 10 --lr_ortho 0.01 \
+
+wait
+
+python closed_form_factorization.py --out ./factor/factor_b8_2000_lr001_id3.pt ./checkpoint_FFHQ_lr0.01_b8_id3/552000.pt --is_ortho --diag_size 10 \
+
+wait
+
+python ppl_sefa.py ./checkpoint_FFHQ_lr0.01_b8_id3/552000.pt --factor ./factor/factor_b8_2000_lr001_id3.pt --ortho_id -3 --sampling full --eps 1.0 \
+
+wait
+
+python ppl.py ./checkpoint_FFHQ_lr0.01_b8_id3/552000.pt --ortho_id -3 --sampling full \
+
+wait
+
+python test_VP_pairs.py --ortho_id -3 --size 256 \
+        --ckpt ./checkpoint_FFHQ_lr0.01_b8_id3/552000.pt \
+        --factor ./factor/factor_b8_2000_lr001_id3.pt --output_dir output_vp_pairsb8_001 &
 
 wait
